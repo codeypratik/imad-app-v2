96759013310e0f315f6d67d0f62c9a72bfcedf60
codeypratik <chavanpratik.pc@gmail.com>
2017-02-06 18:37:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,51 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title: 'Article One~Pratik Chavan',
+    heading: 'Article One',
+    date:'Feb 06 2017',
+    content:'  <p>This is content for my article one his is content for my article oneThis is content for my article oneThis is content for my article one his is content for my article one>This is content for my article one>This is content for my article onepThis is content for my article onThis is content for my article one>This is content for my article oneThis is content for my article one>This is content for my article one>This is content for my article one</p>'
+};
+
+function createTemplate(data){
+    var heading = data.heading;
+    var title = data.title;
+    var content = data.content;
+    var time = data.time;
+
+var htmlTemplate =
+` <html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+           <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+         ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+           <div>
+                ${content}
+           </div>
+           </div>
+        
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +59,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 app.get('/article-two',function(req, res){
     res.send('Articel two requested and will be served here');
