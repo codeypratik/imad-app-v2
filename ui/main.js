8641d8faaf2b736debde9fcd2cf3301436fcb982
codeypratik <chavanpratik.pc@gmail.com>
2017-02-10 13:34:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,9 @@
-console.log('Loaded!');
-//moving image 
+var button = document.getElementById('counter');
+var counter=0;
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+button.onclick = function(){
+    
+    counter = counter+1;
+    var span = documne.getElementById('count');
+    span.innerHTML = counter.toString();
 }
-
-   img.onclick = function(){
-   var interval = setInterval(moveRight, 50);
-};
