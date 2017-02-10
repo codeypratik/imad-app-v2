7fb32f829d090e2f30b74e19b70436f345a343d5
codeypratik <chavanpratik.pc@gmail.com>
2017-02-10 23:13:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names=[];
 app.get('/submit-name/:name',function(req,res){
     var name = req.params.name;
-    names.puch(name);
+    names.push(name);
     res.send(JSON.stringify(names));
 });
 
