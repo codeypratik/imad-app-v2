54f6faf62535c35cbf47b7e9e5083b244d1daebc
codeypratik <chavanpratik.pc@gmail.com>
2017-02-10 12:08:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,12 @@ console.log('Loaded!');
 //moving image 
 
 var img = document.getElementById('madi');
-img.onclick=function(){
-    img.style.marginLeft='100px';
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
+   img.onclick = function(){
+   var interval = setInterval(moveRight, 100);
 };
