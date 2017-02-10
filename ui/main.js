7db2c5c5bc9f8ec9688c41bb40848ff7ff8de41d
codeypratik <chavanpratik.pc@gmail.com>
2017-02-10 22:44:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,4 +19,21 @@ request.open('GET','http://codeypratik.imad.hasura-app.io/counter', true);
 request.send(null);
 };
 
+   //submit name
+    var nameInput=document.getElementById('name');
+    var name=newInput.value;
+    var submit=document.getElementById('submit_btn');
+    
+    submit.onclick = function(){
+        //make a request to the server and send the name
+        //capture a list of names and render it as list
+        
+        var name=['name1','name2','name3'];
+        var list='';
+        for(var i=0;i<names.length;i++){
+            list+='<li>'+names[i]+'</li>'
+        }
+        var ul=document.getElementById('nameslist');
+        ul.innerHTML=list;
+    };
    
